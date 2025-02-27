379913c8b837d799945c81d8b6524dec6568bd61
Abdulaziz Ghuloum <aghuloum@gmail.com>
2025-02-27 11:17:42 +0300

    added engine-io

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import * as eio from "engine.io-client";
 
-createRoot(document.getElementById('root')!).render(
+const url = location.protocol + "//" + location.hostname + ":" + location.port;
+console.log({ url });
+
+const connection = new eio.Socket(url);
+
+connection.on("error", (error) => {
+  console.error(error);
+});
+
+connection.on("open", () => {
+  console.log("open");
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
