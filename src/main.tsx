c903618c81c13d046a87da78814b07384569a006
Abdulaziz Ghuloum <aghuloum@gmail.com>
2025-05-05 12:40:39 +0300

    added password encryption/decryption functions

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { gen_password, verify_password } from "./crypto";
+import { assert } from "./assert.ts";
 
 const darkTheme = createTheme({
   palette: {
@@ -16,3 +18,20 @@ createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </ThemeProvider>
 );
+
+function test() {
+  const t0 = Date.now();
+  const hashed = gen_password("mypass");
+  const verified = verify_password("mypass", hashed);
+  assert(verified === true);
+  const t1 = Date.now();
+  verify_password("mypass", hashed);
+  const t2 = Date.now();
+  console.log({
+    hashed,
+    verified,
+    time1: `${t1 - t0}ms`,
+    time2: `${t2 - t1}ms`,
+  });
+}
+test();
