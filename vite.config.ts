5bde755cece4d72137613940d6abdd257fe3a9db
Abdulaziz Ghuloum <aghuloum@gmail.com>
2025-05-07 11:45:25 +0300

    added vite typechecking module

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        tsconfigPath: "./tsconfig.json",
+        buildMode: false, // enable/disable debugging in browser
+      },
+    }),
+  ],
   server: {
     host: "0.0.0.0",
     port: 4000,
